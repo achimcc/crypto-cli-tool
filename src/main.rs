adeb74405fd618978e564f4acc8931eb2a9ebf5a
achimcc <achim.schneider@posteo.de>
2024-08-03 17:28:35 +0200

    error handling, BufReader

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use aes_siv::{
     aead::{Aead, KeyInit, OsRng},
     Aes256SivAead, Key, Nonce,
 };
+use anyhow::{anyhow, Context, Result};
 use clap::{Parser, ValueEnum};
 use std::{fs, path::PathBuf};
 
@@ -44,32 +45,34 @@ enum Mode {
     Decrypt,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
     let pass_vec = pad_password(cli.password);
     let key: &Key<Aes256SivAead> = pass_vec.as_slice().into();
-    let cipher = Aes256SivAead::new(&key);
-    let input = fs::read(cli.input).expect("Failed to read from input file");
+    let cipher = Aes256SivAead::new(key);
+    let input = fs::read(cli.input).context("Failed to read from input file")?;
     match cli.mode {
         Mode::Encrypt => {
             let nonce = &generate_nonce();
-            let mut output: Vec<u8> = Vec::new();
+            let mut file = std::io::BufWriter::new(
+                std::fs::File::create(cli.output).context("Failed to create output file")?,
+            );
             // We write the generated nonce into the first 16 bytes of the output file
-            let _ = std::io::copy(&mut nonce.as_slice(), &mut output);
+            std::io::copy(&mut nonce.as_slice(), &mut file)?;
             let ciphertext = cipher
                 .encrypt(nonce, &input[..])
-                .expect("Encryption failed");
-            let _ = std::io::copy(&mut &ciphertext[..], &mut output);
-            fs::write(cli.output, output).expect("Failed to write output file");
+                .map_err(|e| anyhow!("Error encrypting plaintext {}", e))?;
+            std::io::copy(&mut &ciphertext[..], &mut file)?;
         }
         Mode::Decrypt => {
             // First 16 bytes are the used nonce
             let (nonce, input) = input.split_at(16);
-            let nonce = &Nonce::from_slice(&nonce).to_owned();
+            let nonce = &Nonce::from_slice(nonce).to_owned();
             let output = cipher
-                .decrypt(nonce, &input[..])
-                .expect("Decryption failed");
-            fs::write(cli.output, output).expect("Failed to write output file");
+                .decrypt(nonce, input)
+                .map_err(|e| anyhow!("Error decrypting plaintext {}", e))?;
+            fs::write(cli.output, output).context("Failed to write output file")?;
         }
     }
+    Ok(())
 }
